601330f731041629fffac09d13f24680ef172093
asifjahir <asifmirza4322@gmail.com>
2026-06-20 17:42:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,56 @@ import KPIHeader from './components/KPIHeader';
 import MapComponent from './components/MapComponent';
 import NewsEngine from './components/NewsEngine';
 import TradingWidget from './components/TradingWidget';
+import LiveBroadcast from './components/LiveBroadcast';
 
 export default function App() {
   return (
     <div className="flex h-screen bg-[#050608] text-slate-300 font-mono overflow-hidden">
       <Sidebar />
-      <main className="flex-1 flex flex-col p-4 gap-4 overflow-y-auto">
+
+      <main className="flex-1 flex flex-col overflow-hidden">
+
+        {/* Top KPI ticker */}
         <KPIHeader />
-        <div className="grid grid-cols-12 gap-4 flex-1">
-          <div className="col-span-8 h-[60vh] border border-white/10 bg-[#0a0d14]">
-            <MapComponent />
+
+        {/* Main content area */}
+        <div className="flex flex-1 overflow-hidden">
+
+          {/* LEFT: Map takes full left panel */}
+          <div className="flex flex-col flex-1 overflow-hidden">
+
+            {/* Map Panel */}
+            <div className="flex-1 border border-white/10 bg-[#0a0d14] relative">
+              <div className="absolute top-3 left-3 z-[1000] text-[10px] tracking-widest text-slate-400">
+                TACTICAL SECTOR ANALYSIS // ACTIVE
+              </div>
+              <MapComponent />
+            </div>
+
+            {/* Bottom: Trading Widget */}
+            <div className="h-[200px] border-t border-white/10">
+              <TradingWidget />
+            </div>
+
           </div>
-          <div className="col-span-4 h-[60vh] border border-white/10 bg-[#0a0d14]">
-            <NewsEngine />
+
+          {/* RIGHT: Live Broadcast + News stacked */}
+          <div className="w-[380px] flex flex-col border-l border-white/10">
+
+            {/* Live Broadcast Player */}
+            <div className="h-[320px] border-b border-white/10 bg-[#0a0d14]">
+              <LiveBroadcast />
+            </div>
+
+            {/* News Engine fills the rest */}
+            <div className="flex-1 overflow-y-auto bg-[#0a0d14]">
+              <NewsEngine />
+            </div>
+
           </div>
-        </div>
-        <div className="h-[25vh]">
-          <TradingWidget />
+
         </div>
       </main>
     </div>
-  );
-}
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
-export default function MapComponent() {
-  return (
-    <MapContainer center={[20, 0]} zoom={2} className="h-full w-full grayscale contrast-[1.5]">
-      {/* Dark theme tile layer */}
-      <TileLayer url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png" />
-      <Marker position={[26.56, 56.25]}>
-        <Popup>Strait of Hormuz - High Risk</Popup>
-      </Marker>
-    </MapContainer>
   );
 }
